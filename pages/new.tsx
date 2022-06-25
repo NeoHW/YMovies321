a275c96d806c05d66af62483bb45927b18162fb2
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-25 20:29:34 +0200

    Store the new post in database

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,19 +1,42 @@
+import { useRouter } from 'next/router';
 import { FormEvent, useRef } from 'react';
 import Form_Fields from '../components/_form_fields';
+
+import { marked } from 'marked';
+import sanitizeHtml from 'sanitize-html';
+import slugify from 'slugify';
+import moment from 'moment';
+
+import { collection, getFirestore, addDoc } from 'firebase/firestore';
 import { useAuthContext } from '../context/AuthContext';
+import firebaseApp from '../firebase';
 import { AuthContext } from '../types/context';
+
+const db = getFirestore(firebaseApp);
+const postRef = collection(db, 'posts');
 
 export default function NewArticle() {
   const title = useRef<HTMLInputElement>(null);
   const description = useRef<HTMLInputElement>(null);
   const content = useRef<HTMLInputElement>(null);
   const { user, login }: AuthContext = useAuthContext();
+  const router = useRouter();
 
   const createNewArticle = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(title.current?.value);
-    console.log(description.current?.value);
-    console.log(content.current?.value);
+    addDoc(postRef, {
+      createdAt: moment().format('MMM Do [at] h:mmA'),
+      creatorId: user.uid,
+      description: description.current?.value,
+      html: sanitizeHtml(marked.parse(content.current?.value as string)),
+      markdown: content.current?.value,
+      slug: slugify(title.current?.value as string, {
+        lower: true,
+        strict: true,
+      }),
+      title: title.current?.value,
+    });
+    router.push('/');
   };
 
   if (user === null) login({ popup: false });
