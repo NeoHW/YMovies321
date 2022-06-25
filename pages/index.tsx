e6a5f6c1bd9dbb4c00ba0cd75461dc3ac1dea45e
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-25 11:32:23 +0200

    It is able to display all the articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,80 @@
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { Article } from '../types/article';
+import { collection, getFirestore, onSnapshot } from 'firebase/firestore';
+import firebaseApp from '../firebase';
 import { useAuthContext } from '../context/AuthContext';
+import type { User } from 'firebase/auth';
+
+const db = getFirestore(firebaseApp);
+const postRef = collection(db, 'posts');
+
+function createNewArticle() {
+  return;
+}
+
+function deleteArticle(articleId: string) {
+  return;
+}
 
 const Home: NextPage = () => {
-  const { user } = useAuthContext();
-  const router = useRouter();
-  if (!user) {
-    router.push('/login');
-    return <>hello</>;
-  }
+  const [articles, setArticles] = useState<Article[]>([] as Article[]);
+
+  onSnapshot(postRef, (event) => {
+    // @ts-ignore
+    const articles: Article[] = event.docs.map((doc) => doc.data());
+    setArticles(articles);
+  });
+
+  const { user }: { user: User } = useAuthContext();
+
   return (
-    <div>
-      Uid: {user.uid} <br />
-      Email: {user.email} <br />
-      DisplayName: {user.displayName} <br />
-    </div>
+    <>
+      <h1 className="mb-4">Blog Articles</h1>
+      <button
+        id="new-article"
+        className="btn btn-success"
+        onClick={createNewArticle}
+      >
+        Create New Article
+      </button>
+      {articles.map((article: Article) => {
+        return (
+          <div className="card mt-4" key={article.id}>
+            <div className="card-body">
+              <h4 className="card-title">{article.title}</h4>
+              <div className="card-subtitle text-muted mb-2">
+                {article.createdAt}
+              </div>
+              <div className="card-text mb-2">{article.description}</div>
+              <a href={`articles/${article.slug}`} className="btn btn-primary">
+                Read More
+              </a>
+              {article.creatorId !== user?.uid ? (
+                ''
+              ) : (
+                <span className="creator" id={article.creatorId}>
+                  {' '}
+                  <a
+                    href={`articles/edit/${article.id}`}
+                    className="btn btn-info"
+                  >
+                    Edit
+                  </a>{' '}
+                  <button
+                    type="submit"
+                    className="btn btn-danger"
+                    onClick={() => deleteArticle(article.id)}
+                  >
+                    Delete
+                  </button>
+                </span>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
