0921f515d6f424885e72b9c98f405c7dc662889a
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-25 19:08:43 +0200

    Redirect user to login page if the user hasn't signed in

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useRef } from 'react';
 import Form_Fields from '../components/_form_fields';
+import { useAuthContext } from '../context/AuthContext';
 
 const article = {
   id: 'id',
@@ -16,17 +17,20 @@ export default function NewArticle() {
   const description = useRef<HTMLInputElement>(null);
   const content = useRef<HTMLInputElement>(null);
 
-  const newArticle = (event: FormEvent<HTMLFormElement>) => {
+  const createNewArticle = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(title.current?.value);
     console.log(description.current?.value);
     console.log(content.current?.value);
   };
+  const { user, login } = useAuthContext();
+
+  if (user === null) login({ popup: false });
 
   return (
     <>
       <h1 className="mb-4">Create New Article</h1>
-      <form onSubmit={newArticle}>
+      <form onSubmit={createNewArticle}>
         <Form_Fields
           article={null}
           // @ts-ignore
