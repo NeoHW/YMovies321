e3dc82fcc3b210e8d7d0602131002887198320b8
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-26 11:35:28 +0200

    Added a way to allow the user to view the entire article

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -8,12 +8,18 @@ import {
 import { useRouter } from 'next/router';
 import { Article } from '../../types/article';
 import firebaseApp from '../../firebase';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const db = getFirestore(firebaseApp);
 const postRef = collection(db, 'posts');
 
 export default function ViewArticle() {
   const router = useRouter();
+  const [createdAt, setCreatedAt] = useState('');
+  const [html, setHtml] = useState('');
+  const [title, setTitle] = useState('');
+  const [articleId, setArticleId] = useState('');
   const { slug: givenSlug } = router.query;
 
   const post = query(postRef, where('slug', '==', givenSlug));
@@ -21,17 +27,25 @@ export default function ViewArticle() {
     event.docs.forEach((doc) => {
       // @ts-ignore
       const postData: Article = { ...doc.data(), id: doc.id };
-      const {
-        id,
-        createdAt,
-        creatorId,
-        description,
-        html,
-        markdown,
-        slug,
-        title,
-      } = postData;
+      const { createdAt, html, title, id }: Article = postData;
+
+      setCreatedAt(createdAt);
+      setHtml(html);
+      setArticleId(id);
+      setTitle(title);
     });
   });
-  return <>Post:</>;
+  return (
+    <>
+      <h1 className="mb-1">{title}</h1>
+      <div className="text-muted mb-2">{createdAt}</div>
+      <Link href="/">
+        <a className="btn btn-secondary">All Articles</a>
+      </Link>{' '}
+      <a href={`/articles/edit/${articleId}`} className="btn btn-info">
+        Edit
+      </a>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+    </>
+  );
 }
