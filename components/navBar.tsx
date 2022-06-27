6a4131b9d00346534e261780bd41059ff087b8ae
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-27 11:40:10 +0200

    Working on navbar

---
+++ components/navBar.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import Link from 'next/link';
+import { useAuthContext } from '../context/AuthContext';
+import { useRouter } from 'next/router';
+
+const NavbarComp = () => {
+  const { user, logout, login } = useAuthContext();
+  const router = useRouter();
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Link href="/" passHref>
+          <Navbar.Brand>Blog Articles</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {user ? (
+              <div>
+                <Nav.Link
+                  onClick={() => {
+                    logout();
+                    router.push('/');
+                  }}
+                >
+                  Logout
+                </Nav.Link>
+              </div>
+            ) : (
+              // Show `login` and `Signup` link if the user hasn't logged in yet
+              <>
+                <Nav.Link
+                  onClick={() => {
+                    login({ popup: true });
+                    router.push('/');
+                  }}
+                >
+                  Signup
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    login({ popup: true });
+                    router.push('/');
+                  }}
+                >
+                  Login
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComp;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
+import NavBar from '../components/navBar';
 import { AuthContextProvider } from '../context/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // return <Component {...pageProps} />;
   return (
     <AuthContextProvider>
+      <NavBar />
       <div className="container">
         <Component {...pageProps} />
       </div>
