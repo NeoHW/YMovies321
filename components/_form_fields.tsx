7a054baf4acf9377798371a1af044a403bb1a372
JagTheFriend <JagTheFriend12@gmail.com>
2022-07-02 19:50:36 +0200

    Updated files

--- components/_form_fields.tsx
+++ components/_form_fields.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import type { MutableRefObject } from 'react';
 import { useState } from 'react';
 import { Article } from '../types/article';
-import Preview from './preview';
 
 export default function FormFields({
   article,
@@ -15,84 +14,71 @@ export default function FormFields({
   description: MutableRefObject<HTMLTextAreaElement | null>;
   content: MutableRefObject<HTMLTextAreaElement | null>;
 }) {
-  const [previewForm, setPreviewForm] = useState(false);
   const [previewBtn, setPreviewBtn] = useState(false);
   return (
     <>
-      {previewForm ? (
-        <Preview />
+      <h1 className="mb-4">
+        {article === null ? 'Create New Article' : 'Edit Article'}
+      </h1>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          className="form-control"
+          placeholder="Title of the article"
+          defaultValue={article?.title}
+          onChange={() => {
+            setPreviewBtn(true);
+          }}
+          ref={title}
+          required
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <label htmlFor="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          className="form-control"
+          placeholder="A small description of the article"
+          ref={description}
+          defaultValue={article?.description}
+          required
+        />
+      </div>
+      <br />
+      <div className="form-group">
+        <label htmlFor="markdown">Content</label>
+        <textarea
+          name="markdown"
+          id="markdown"
+          className="form-control"
+          onChange={() => {
+            setPreviewBtn(true);
+          }}
+          defaultValue={article?.markdown}
+          ref={content}
+          placeholder="Content of your article. You may use markdown for styling"
+          required
+        />
+      </div>
+      <br />
+      <Link href="/">
+        <a className="btn btn-secondary">Cancel</a>
+      </Link>{' '}
+      {previewBtn ? (
+        <button type="button" className="btn btn-info">
+          Preview
+        </button>
       ) : (
-        <>
-          <h1 className="mb-4">
-            {article === null ? 'Create New Article' : 'Edit Article'}
-          </h1>
-          <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="form-control"
-              placeholder="Title of the article"
-              defaultValue={article?.title}
-              onChange={() => {
-                setPreviewBtn(true);
-              }}
-              ref={title}
-              required
-            />
-          </div>
-          <br />
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
-              id="description"
-              className="form-control"
-              placeholder="A small description of the article"
-              ref={description}
-              defaultValue={article?.description}
-              required
-            />
-          </div>
-          <br />
-          <div className="form-group">
-            <label htmlFor="markdown">Content</label>
-            <textarea
-              name="markdown"
-              id="markdown"
-              className="form-control"
-              onChange={() => {
-                setPreviewBtn(true);
-              }}
-              defaultValue={article?.markdown}
-              ref={content}
-              placeholder="Content of your article. You may use markdown for styling"
-              required
-            />
-          </div>
-          <br />
-          <Link href="/">
-            <a className="btn btn-secondary">Cancel</a>
-          </Link>{' '}
-          {previewBtn ? (
-            <>
-              <button
-                type="button"
-                className="btn btn-info"
-                onClick={() => setPreviewForm(true)}
-              >
-                Preview
-              </button>
-            </>
-          ) : (
-            ''
-          )}{' '}
-          <button type="submit" className="btn btn-primary">
-            {article !== null ? 'Save' : 'Create'}
-          </button>
-        </>
-      )}
+        ''
+      )}{' '}
+      <button type="submit" className="btn btn-primary">
+        {article !== null ? 'Save' : 'Create'}
+      </button>
     </>
   );
 }
--- components/preview.tsx
+++ components/preview.tsx
@@ -1,3 +1,13 @@
-export default function Preview() {
+import type { MutableRefObject } from 'react';
+
+export default function Preview({
+  title,
+  description,
+  content,
+}: {
+  title: MutableRefObject<HTMLInputElement | null>;
+  description: MutableRefObject<HTMLTextAreaElement | null>;
+  content: MutableRefObject<HTMLTextAreaElement | null>;
+}) {
   return <></>;
 }
