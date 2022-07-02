0ff4d0fee2d7fc6425e385bba104b21f1d2e52d6
JagTheFriend <JagTheFriend12@gmail.com>
2022-07-02 17:16:46 +0200

    Use the useRef hook

--- components/_form_fields.tsx
+++ components/_form_fields.tsx
@@ -1,9 +1,8 @@
-import { marked } from 'marked';
-import moment from 'moment';
 import Link from 'next/link';
-import { useRef, useState } from 'react';
-import sanitizeHtml from 'sanitize-html';
+import type { MutableRefObject } from 'react';
+import { useState } from 'react';
 import { Article } from '../types/article';
+import Preview from './preview';
 
 export default function FormFields({
   article,
@@ -12,47 +11,16 @@ export default function FormFields({
   content,
 }: {
   article: Article | null;
-  title: (val: string) => any;
-  description: (val: string) => any;
-  content: (val: string) => any;
+  title: MutableRefObject<HTMLInputElement | null>;
+  description: MutableRefObject<HTMLTextAreaElement | null>;
+  content: MutableRefObject<HTMLTextAreaElement | null>;
 }) {
   const [previewForm, setPreviewForm] = useState(false);
   const [previewBtn, setPreviewBtn] = useState(false);
-  const [title_, setTitle_] = useState(
-    article ? article.title : 'Title of your article'
-  );
-  const [content_, setContent_] = useState(
-    article ? article.description : 'Content of your article'
-  );
-
   return (
     <>
       {previewForm ? (
-        <>
-          <h1 className="mb-1">{title_}</h1>
-          <div className="text-muted mb-2">
-            {moment().format('MMM Do [at] h:mmA')}
-          </div>
-          <Link href="/">
-            <a className="btn btn-secondary">All Articles</a>
-          </Link>{' '}
-          <a href={`#`} className="btn btn-info">
-            Edit
-          </a>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: sanitizeHtml(marked.parse(content_)),
-            }}
-          ></div>{' '}
-          {/* Allow the user to turn preview off */}
-          <button
-            type="button"
-            className="btn btn-info"
-            onClick={() => setPreviewForm(false)}
-          >
-            Turn off preview
-          </button>{' '}
-        </>
+        <Preview />
       ) : (
         <>
           <h1 className="mb-4">
@@ -67,11 +35,10 @@ export default function FormFields({
               className="form-control"
               placeholder="Title of the article"
               defaultValue={article?.title}
-              onChange={(event) => {
-                title(event.target.value);
-                setTitle_(event.target.value);
+              onChange={() => {
                 setPreviewBtn(true);
               }}
+              ref={title}
               required
             />
           </div>
@@ -83,11 +50,8 @@ export default function FormFields({
               id="description"
               className="form-control"
               placeholder="A small description of the article"
+              ref={description}
               defaultValue={article?.description}
-              onChange={(event) => {
-                description(event.target.value);
-                // setPreviewBtn(true);
-              }}
               required
             />
           </div>
@@ -98,12 +62,11 @@ export default function FormFields({
               name="markdown"
               id="markdown"
               className="form-control"
-              onChange={(event) => {
-                content(event.target.value);
-                setContent_(event.target.value);
+              onChange={() => {
                 setPreviewBtn(true);
               }}
               defaultValue={article?.markdown}
+              ref={content}
               placeholder="Content of your article. You may use markdown for styling"
               required
             />
