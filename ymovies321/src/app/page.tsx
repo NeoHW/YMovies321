94010d55dc99033c8ec250886c63cca9c8511512
Ang Leng Khai <lengkhai@gmail.com>
2023-05-24 14:01:26 +0800

    trying to add data to firebase

--- ymovies321/src/app/page.tsx
+++ ymovies321/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
+import addData from './firebase/firestore/addData'
+import getData from './firebase/firestore/getData'
+
 
 export default function Home() {
+  addData("users", "TJlfmor2MUtrbfK0wHA9", {"name" : "lengkhai"})
+
   return (
     <h1> login </h1>
   )
