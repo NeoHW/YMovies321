64df66de6e2a35a128c14c4ec088e447dd17a7da
NeoHW <neohaowei@gmail.com>
2023-05-24 22:11:38 +0800

    clean up
    still error 404

---
+++ ymovies321/src/app/types/article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+    id?: string;
+    createdAt: string;
+    creatorId: string;
+    description: string;
+    html: string;
+    markdown: string;
+    slug: string;
+    title: string;
+  }
