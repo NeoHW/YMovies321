9ff1f8c59f299ef710146374fb7ac599241a5ba7
NeoHW <neohaowei@gmail.com>
2023-05-24 22:56:43 +0800

    going back to starting guide

--- ymovies321/src/app/types/context.ts
+++
@@ -1,7 +0,0 @@
-import type { User, UserCredential } from 'firebase/auth';
-
-export interface AuthContext {
-  user: User;
-  login: ({ popup }: { popup: boolean }) => Promise<UserCredential>;
-  logout: () => Promise<void>;
-}
