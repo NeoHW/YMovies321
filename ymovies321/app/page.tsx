ea6ce7b558a97a3e7cb4a7b0dbfaa3f87e093b24
NeoHW <neohaowei@gmail.com>
2023-05-26 02:05:02 +0800

    did favicon icon

--- ymovies321/app/page.tsx
+++ ymovies321/app/page.tsx
@@ -4,9 +4,9 @@ import Navbar from "./Navigation/NavbarSignedOut";
 export default function Home() {
   return (
     <div>
-        <div>
-          <Navbar />
-        </div>
+      <div>
+        <Navbar />
+      </div>
       <h1 className="text-3xl font-bold underline">Home Page</h1>
       <p>Some content</p>
     </div>
