4969f8d108224bd2f15d5d528b96f19824c25c2b
NeoHW <neohaowei@gmail.com>
2023-05-25 22:38:38 +0800

    initialised tailwind with next,, created template navbar

--- ymovies321/app/page.tsx
+++ ymovies321/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Image from 'next/image'
+import Navbar from "./Navigation/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+        <div>
+          <Navbar />
+        </div>
+      <h1 className="text-3xl font-bold underline">Home Page</h1>
       <p>Some content</p>
     </div>
   );
