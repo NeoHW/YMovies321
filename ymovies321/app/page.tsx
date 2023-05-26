5e9d576cf148dfbf5c348ce77e6dca0b056dbc61
Ang Leng Khai <lengkhai@gmail.com>
2023-05-26 15:19:48 +0800

    streamlined signin/signout navbar

--- ymovies321/app/page.tsx
+++ ymovies321/app/page.tsx
@@ -5,24 +5,23 @@ import 'firebase/firestore';
 
 import Image from 'next/image'
 import firebase_app from './firebase/config';
-import NavbarSignedOut from "./Navigation/NavbarSignedOut";
-import NavbarSignedIn from "./Navigation/NavbarSignedIn";
+import Navbar from "./components/Navbar";
 import { useState } from "react";
 import { signIn } from "./authContext/auth";
 
 
 
 export default function Home() {
-  
+
   {
-  /* trying to move it to auth.ts
-  const provider = new GoogleAuthProvider()
-  const auth = getAuth(firebase_app);
-  const signIn = async() => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result.user);
-  }
-  */
+    /* trying to move it to auth.ts
+    const provider = new GoogleAuthProvider()
+    const auth = getAuth(firebase_app);
+    const signIn = async() => {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result.user);
+    }
+    */
   }
 
   {
@@ -32,31 +31,38 @@ export default function Home() {
       const user = await signIn();
       // Perform any additional actions after sign-in if needed
       console.log("signed in")
-    
+
       // Pass the user data to somewhere
       setProfile(user.user.displayName);
     };
-  
+
+    const handleSignOut = async () => {
+      setProfile(null);
+    }
+
     return (
       <div>
-          <div>
-            {
+        <div>
+          {
             /* user ? <NavbarSignedIn /> : <NavbarSignedOut /> */
-            }
-            {
-              // < NavbarSignedOut />
-            }
-          </div>
-          <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  onClick={handleSignIn} // call signIn function on button click
-                >
-                  <span className="sr-only">Log In</span>
-                  Log In 
-                </button>
+          }
+          {
+            <div>
+            </div>
+          }
+        </div>
+  
+        {profile == null ? (
+          <Navbar isSignedIn={false} user={profile} handleSignIn={handleSignIn} handleSignOut = {handleSignOut} />
+        )
+          : (
+            <Navbar isSignedIn={true} user={profile} handleSignIn={handleSignIn} handleSignOut = {handleSignOut} />
+          )}
+
+
         <h1 className="text-3xl font-bold underline">Home Page</h1>
-        <p>Username: {profile}</p>
+        {profile == null ? (<p>LOGGED OUT</p>) : (<p>LOGGED IN: {profile}</p>)}
+
       </div>
     );
   }
