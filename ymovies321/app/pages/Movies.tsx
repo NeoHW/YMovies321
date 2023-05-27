4db271c392e609f3f0404974eb778459c4acf051
NeoHW <neohaowei@gmail.com>
2023-05-28 04:25:46 +0800

    added sample code for adding data

--- ymovies321/app/pages/Movies.tsx
+++ ymovies321/app/pages/Movies.tsx
@@ -1,12 +1,41 @@
 // shows trending movies + searchbar(?)
 import firebase_app from "../firebase/config";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { collection, doc, getDoc, setDoc, getFirestore } from "firebase/firestore";
 
 
+// initialise cloud firestone and get ref to service
+const db = getFirestore(firebase_app);
+
+// https://firebase.google.com/docs/firestore/manage-data/add-data
+async function addData() {    
+    // "cities" is the new collection's name
+    const citiesRef = collection(db, "cities");
+
+    // adding separate documents into the collection (e.g. "SF")
+    await setDoc(doc(citiesRef, "SF"), {
+        name: "San Francisco", state: "CA", country: "USA",
+        capital: false, population: 860000,
+        regions: ["west_coast", "norcal"] }); // this is adding an array
+    await setDoc(doc(citiesRef, "LA"), {
+        name: "Los Angeles", state: "CA", country: "USA",
+        capital: false, population: 3900000,
+        regions: ["west_coast", "socal"] });
+    await setDoc(doc(citiesRef, "DC"), {
+        name: "Washington, D.C.", state: null, country: "USA",
+        capital: true, population: 680000,
+        regions: ["east_coast"] });
+    await setDoc(doc(citiesRef, "TOK"), {
+        name: "Tokyo", state: null, country: "Japan",
+        capital: true, population: 9000000,
+        regions: ["kanto", "honshu"] });
+    await setDoc(doc(citiesRef, "BJ"), {
+        name: "Beijing", state: null, country: "China",
+        capital: true, population: 21500000,
+        regions: ["jingjinji", "hebei"] });
+}
+
+// https://firebase.google.com/docs/firestore/query-data/get-data
 async function fetchData() {
-    // initialise cloud firestone and get ref to service
-    const db = getFirestore(firebase_app);
-
     // getting data
     const docRef = doc(db, "MoviesDB", "Sherlock Holmes");
     const docSnap = await getDoc(docRef);
@@ -21,6 +50,7 @@ async function fetchData() {
 
 function Movies() {
     // testing out fetchData from database function (it works!)
+    addData();
     fetchData();
     
     // return jsx
