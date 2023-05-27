4d13166e4e493451deec6235b6cc2bb867051fab
NeoHW <neohaowei@gmail.com>
2023-05-28 03:55:12 +0800

    fetchData function that retrieves data from firebase

--- ymovies321/app/pages/Movies.tsx
+++ ymovies321/app/pages/Movies.tsx
@@ -1,7 +1,29 @@
 // shows trending movies + searchbar(?)
+import firebase_app from "../firebase/config";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+
+
+async function fetchData() {
+    // initialise cloud firestone and get ref to service
+    const db = getFirestore(firebase_app);
+
+    // getting data
+    const docRef = doc(db, "MoviesDB", "Sherlock Holmes");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+    }
+}
 
 function Movies() {
-
+    // testing out fetchData from database function (it works!)
+    fetchData();
+    
+    // return jsx
     return (
         <div>
             movies will be shown here
