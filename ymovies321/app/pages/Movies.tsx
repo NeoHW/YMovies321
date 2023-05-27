4987880aaaa4e1d389d2c55517d8ff8d55cc36eb
NeoHW <neohaowei@gmail.com>
2023-05-27 19:13:31 +0800

    fixed some types; created blank movies component

---
+++ ymovies321/app/pages/Movies.tsx
@@ -0,0 +1,12 @@
+// shows trending movies + searchbar(?)
+
+function Movies() {
+
+    return (
+        <div>
+            movies will be shown here
+        </div>
+    )
+};
+
+export default Movies;
