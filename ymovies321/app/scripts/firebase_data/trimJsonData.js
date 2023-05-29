84aaf6f8b38b80c4ff9ab4e49480d22811222135
NeoHW <neohaowei@gmail.com>
2023-05-30 03:46:56 +0800

    scripts works,, 1k movies left aft filter popularity > 50
    need at least 20k

--- ymovies321/app/scripts/firebase_data/trimJsonData.js
+++ ymovies321/app/scripts/firebase_data/trimJsonData.js
@@ -6,8 +6,8 @@ const outputFile = './movie_ids_trimmed.json';
 
 // Create a readline interface
 const rl = readline.createInterface({
-  input: createReadStream(inputFile),
-  crlfDelay: Infinity
+    input: createReadStream(inputFile),
+    crlfDelay: Infinity
 });
 
 // Array to store filtered movie entries
@@ -15,26 +15,22 @@ const filteredMovies = [];
 
 // Process each line of the JSON file
 rl.on('line', (jsonLine) => {
-  // Parse the JSON line
-  const movie = JSON.parse(jsonLine);
+    // Parse the JSON line
+    const movie = JSON.parse(jsonLine);
 
-  // Check if the movie's popularity is lower than 50
-  if (movie.popularity < 50) {
+    // Check if the movie's popularity is lower than 50
+    if (movie.popularity < 50) {
     // Skip this movie entry
     return;
-  }
+    }
+    // Convert the movie object to a single-line JSON string
+    const movieJson = JSON.stringify(movie);
 
-  // Add the movie entry to the filteredMovies array
-  filteredMovies.push(movie);
+    // Write the movie JSON to the output file with a newline character
+    writeFileSync(outputFile, movieJson + "\n", { flag: "a" });
 });
 
 // Handle the end of the file
-rl.on('close', () => {
-  // Convert the filteredMovies array to JSON string
-  const filteredJson = JSON.stringify(filteredMovies);
-
-  // Write the filtered JSON data to the output file
-  writeFileSync(outputFile, filteredJson);
-
-  console.log('Filtered JSON file created:', outputFile);
-});
+rl.on("close", () => {
+    console.log("Filtered JSON file created:", outputFile);
+  });
