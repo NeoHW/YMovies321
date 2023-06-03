d81c63ae702f0abb9b7161e8fc389a8b67ef81de
NeoHW <neohaowei@gmail.com>
2023-06-03 17:34:10 +0800

    when signing out, it will lead back to homepage

--- ymovies321/app/components/NavBars/TrendingNavBar.tsx
+++ ymovies321/app/components/NavBars/TrendingNavBar.tsx
@@ -173,7 +173,7 @@ function Navbar({ isSignedIn, profile, handleSignIn, handleSignOut }: ButtonProp
                                                     <Menu.Item>
                                                         {({ active }) => (
                                                             <a
-                                                                href="#"
+                                                                href="/"
                                                                 className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                                 onClick={handleSignOut}
                                                             >
