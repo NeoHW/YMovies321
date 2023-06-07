0b44066fb4a76d1c13769f009bdaec5898a68c16
Ang Leng Khai <lengkhai@gmail.com>
2023-06-07 17:49:13 +0800

    profile page

---
+++ ymovies321/app/components/Profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile({ user }) {
+    return(
+    <div>
+        Profile component: show app specific stats of: {user.displayName} eg. fav movie or whatever
+    </div>
+    )
+}
--- ymovies321/app/pages/profile/[id]/page.tsx
+++ ymovies321/app/pages/profile/[id]/page.tsx
@@ -3,15 +3,37 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from "../../../authContext/auth"
 import Navbar from '../../../components/Navbar';
+import Profile from '../../../components/Profile';
+import { Box, Grid } from '@mui/material';
+import Image from 'mui-image';
+import { Typography } from '@mui/material';
 
 export default function Page() {
     const [user] = useAuthState(auth);
-    return (
+    return user != null ? (
         <div>
-            <Navbar isSignedIn={true} profile={user} nav={null} ></Navbar>
-            <h1>My Profile</h1>
-            <div>{user?.displayName}</div>
-            <img src={user?.photoURL}></img>
+            <Navbar isSignedIn={true} profile={user} nav={null}></Navbar>
+            <Grid
+                container
+                alignItems="center"
+                direction="column"
+                spacing={8}
+            >
+                <Grid item>
+                    <Typography variant="subtitle2" sx={{ fontSize: "32px", fontWeight: "400" }}>
+                        {user.displayName}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Image
+                        width={250}
+                        src={user?.photoURL}
+                    />
+                </Grid>
+                <Grid item>Email: {user.email}</Grid>
+            </Grid>
+
+            <Profile user={user} ></Profile>
         </div>
-    );
+    ) : <div></div>;
 }
