36058cb0377f284bb7b7d79d490ca7886d198414
Ang Leng Khai <lengkhai@gmail.com>
2023-06-15 19:08:19 +0800

    update movie detail page

--- ymovies321/app/components/MovieDetails.tsx
+++ ymovies321/app/components/MovieDetails.tsx
@@ -1,9 +1,6 @@
-import Link from 'next/link';
-import { Typography, Box, Grid } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 import moment from "moment";
 import { MovieResult } from "../interfaces/TMDBapi";
-import Image from 'mui-image';
-import { toASCII } from 'punycode';
 
 
 
@@ -13,14 +10,14 @@ export default function MovieDetails({ item }: { item: MovieResult }) {
         <div>
             <Grid container direction="column" alignItems="center">
                 <Grid
-                item>
+                    item>
                     <Typography p={3} sx={{ color: "#ffffff" }} fontWeight="bold" variant="h4" style={{ display: "-webkit-box", WebkitLineClamp: "1", WebkitBoxOrient: "vertical", overflow: "hidden", textOverflow: "ellipsis" }}>
                         {item.title ? item.title : item.original_title}
                     </Typography>
                 </Grid>
                 <Grid
-                item
-                sx={{height:"150%"}}
+                    item
+                    sx={{ height: "150%" }}
                 >
                     <img
                         className=""
@@ -34,21 +31,30 @@ export default function MovieDetails({ item }: { item: MovieResult }) {
                     ></img>
                 </Grid>
 
-                <Grid 
-                item
-                className="pl-1">
-
-                    <Typography sx={{ color: "#ffffff" }} variant="subtitle2" >
+                <Grid
+                    item
+                    className="pl-1"
+                >
+                    <Typography sx={{ color: "#ffffff" }} variant="subtitle1" >
                         {moment(item.release_date).format("YYYY")}
-                    </Typography>
-                    <Typography sx={{ color: "#d8dbda" }} variant="subtitle2" >
+                    </Typography></Grid>
+                <Grid
+                    item
+                    className="pl-1"
+                >
+                    <Typography sx={{ color: "#d8dbda" }} variant="subtitle1" >
                         <p>{item.vote_average.toFixed(1)} / 10</p>
                     </Typography>
-                    <Typography sx={{ color: "#ffffff" }} variant="subtitle2">
+                </Grid>
+                
+                    <Typography sx={{ color: "#ffffff" }} variant="subtitle1">
                         <span>Genres: </span>
                         {item.genres.map(g => <span key={g.name}>  {"\u2022 " + g.name} </span>)}
                     </Typography>
-                </Grid>
+                    <Typography maxWidth="50%" sx={{ color: "#d8dbda" }} variant="subtitle2">
+                        {item.overview}
+                    </Typography>
+                    <Typography sx={{ color: "#d8dbda" }}>Runtime: {item.runtime} minutes</Typography>
             </Grid>
         </div>
     )
