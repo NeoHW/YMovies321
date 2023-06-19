90abac9cc258bc3c6fb95aec8e04b4fd03b50244
NeoHW <neohaowei@gmail.com>
2023-06-19 17:40:38 +0800

    when not sign in, requires sign in before posting comments

--- ymovies321/app/authContext/reviews/addReviewToMovieDB.tsx
+++ ymovies321/app/authContext/reviews/addReviewToMovieDB.tsx
@@ -2,14 +2,9 @@ import { doc, updateDoc, arrayUnion, Timestamp} from "firebase/firestore";
 import { User } from "firebase/auth";
 import { findUser } from "../users/findUser";
 import { db } from "../users/reauthenticateUser";
-import { signIn } from "../auth";
 
 export async function addReviewToMovieDB(user: User, movieId: string, review: string) {
     const userRes = await findUser(user);
-
-    if (userRes == null) {
-        signIn();
-    }
 
     if (userRes != null && userRes.size > 0) {
         const uid = user.uid;
