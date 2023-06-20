31c936f7e8bca24fca9b8c65e0a95084ff6944e5
NeoHW <neohaowei@gmail.com>
2023-06-20 22:36:12 +0800

    styling of dropdown menu and remove review button
    TODO:
    try to make it such that when anywhere else on the page is clicked, the dropdown menu disappears

--- ymovies321/app/components/ReviewArticle.tsx
+++ ymovies321/app/components/ReviewArticle.tsx
@@ -72,9 +72,13 @@ export default function ReviewArticle({ user, movieId, handleRefresh, reviewData
                             className="absolute top-full right-0 z-10 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                             <ul className="mt-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
                                 <li>
-                                <Button onClick={() => handleDeleteReview()} className="text-white">
+                                <button 
+                                    type="button"
+                                    onClick={() => handleDeleteReview()} 
+                                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                                >
                                     remove
-                                </Button>
+                                </button>
                                 </li>
                             </ul>
                         </div>
