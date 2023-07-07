3f628d052ae4982254701b6160d26a8038de9bd8
NeoHW <neohaowei@gmail.com>
2023-07-07 18:53:01 +0800

    changed movie details page to use firebase scores

--- ymovies321/app/components/Score.tsx
+++ ymovies321/app/components/Score.tsx
@@ -2,13 +2,12 @@
 import { auth } from '../authContext/auth';
 import { Typography, Rating } from '@mui/material';
 import { useAuthState } from "react-firebase-hooks/auth"
-import { MovieResult } from '../interfaces/TMDBapi';
 import { useState, useEffect } from 'react';
 import playerWriteNewMovieScore from '../authContext/scores/playerWriteNewMovieScore';
 import movieWriteNewScore from '../authContext/scores/movieWriteNewScore';
 
 
-export default function Score({ movie }: { movie: MovieResult }) {
+export default function Score({ firebaseData }: { firebaseData: any }) {
     const [user] = useAuthState(auth);
 
     // Annoyance: after voting, score does not stay at selected score after refreshing
@@ -18,18 +17,18 @@ export default function Score({ movie }: { movie: MovieResult }) {
     return (
         <div>
             <Typography align="center" sx={{ color: "#d8dbda" }} variant="subtitle1" >
-                <p>{movie.vote_average.toFixed(1)} / 10, {movie.vote_count} votes</p>
+                <p>{firebaseData.vote_average.toFixed(1)} / 10, {firebaseData.vote_count} votes</p>
             </Typography>
             <Rating
                 value={value}
                 max={10}
                 onChange={(event, newValue) => {
                     setValue(newValue);
-                    playerWriteNewMovieScore(user, movie.id, newValue)
-                    movieWriteNewScore(user, movie.id, newValue);
+                    playerWriteNewMovieScore(user, firebaseData.id, newValue)
+                    movieWriteNewScore(user, firebaseData.id, newValue);
                 }}
             />
 
-        </div >
+        </div>
     )
 }
