614aed868df32cbb98a74da49da59b56cb06864a
NeoHW <neohaowei@gmail.com>
2023-07-11 01:09:45 +0800

    button ui changes

--- ymovies321/app/pages/profile/[id]/page.tsx
+++ ymovies321/app/pages/profile/[id]/page.tsx
@@ -19,25 +19,24 @@ export default function ProfilePage() {
 
     return user ? (
         <div>
-            <Navbar isSignedIn={true} profile={user} nav={null}></Navbar>
+            <Navbar isSignedIn={true} profile={user} nav={null} />
+            
             <Box sx={{ p: 3 }}>
-                <Grid container spacing={2} alignItems="center">
-                    <Grid item>
-                        <Avatar src={user.photoURL} alt="Profile Image" sx={{ width: 100, height: 100 }} />
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h5">{user.displayName}</Typography>
-                        <Typography variant="subtitle1" color="text.secondary">{user.email}</Typography>
-                    </Grid>
-                </Grid>
+                <div className="flex items-center space-x-4">
+                    <Avatar src={user.photoURL} alt="Profile Image" sx={{ width: 100, height: 100 }} />
+                    <div className="font-medium dark:text-white">
+                        <div> {user.displayName} </div>
+                        <div className="text-sm text-gray-500 dark:text-gray-400"> {user.email} </div>
+                    </div>
+                </div>
             </Box>
 
             <UserProfileInfo user={user} />
 
             <Box sx={{ p: 3 }}>
-                <Button variant="contained" onClick={handleDeleteAccount}>
+                <button type="button" onClick={handleDeleteAccount} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                     Delete Account
-                </Button>
+                </button>
             </Box>
         </div>
     ) : <div></div>;
